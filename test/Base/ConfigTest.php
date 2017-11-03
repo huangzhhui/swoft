775ab5a82fac801f232665aee7a0bdda2d306708
黄朝晖 <huangzh@kkguan.com>
2017-11-03 18:19:30 +0800

    完善UnitTest

---
+++ test/Base/ConfigTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Swoft\Test\Base;
+
+
+use Swoft\App;
+use Swoft\Base\Config;
+use Swoft\Test\AbstractTestCase;
+
+/**
+ * @uses      ConfigTest
+ * @version   2017年11月03日
+ * @author    huangzhhui <huangzhwork@gmail.com>
+ * @copyright Copyright 2010-2016 Swoft software
+ * @license   PHP Version 7.x {@link http://www.php.net/license/3_0.txt}
+ */
+class ConfigTest extends AbstractTestCase
+{
+
+    public function testConfig()
+    {
+        /** @var \Swoft\Base\Config $config */
+        $config = App::getBean('config');
+        $this->assertInstanceOf(Config::class, $config);
+        $value = 1;
+        $offset = 'test';
+        $config->offsetSet($offset, $value);
+        $this->assertEquals($value, $config->offsetGet($offset));
+    }
+}
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 require_once dirname(dirname(__FILE__)) . "/vendor/autoload.php";
 require_once dirname(dirname(__FILE__)) . '/config/define.php';
-$console = new \Swoft\Console\Console();
-$console->run();
+
+// init
+$server = new \Swoft\Server\HttpServer();
+\Swoft\Bean\BeanFactory::reload();
+$initApplicationContext = new \Swoft\Base\InitApplicationContext();
+$initApplicationContext->init();
